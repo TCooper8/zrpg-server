18b87a25964a3ed53c545585f1ead2eb4b44b729
TCooper8 <coopetyl@live.com>
2017-09-04 17:50:59 -0600

    Removed unnused data from Program.fs

--- Program.fs
+++ Program.fs
@@ -1,29 +1,19 @@
-﻿open Zrpg
+﻿open System
+
+open Zrpg
+open Zrpg.Primitives
 open Zrpg.Types
 
 [<EntryPoint>]
 let main argv =
   let connectionString = "Server=localhost;User Id=postgres;Database=postgres;Port=5432"
   use conn = Sql.connect connectionString |> Async.RunSynchronously
-  let itemService: ItemService.Service =
+  let items: ItemService.Service =
     { connect = fun () -> Sql.connect connectionString
     }
 
-  async {
-    do! ItemService.setup itemService
-    printfn "Setup tables"
-    let! reply =
-      ItemService.addArmor itemService
-        { name = "Helmet"
-          description = "None."
-          //damage = 50
-          defense = 50
-        }
-    let (Created id) = reply
-    printfn "Added axe %A" id
-    let! item = ItemService.get itemService id
-    printfn "Got axe %A" item
-  }
-  |> Async.RunSynchronously
+  let blueprints: BlueprintService.Service =
+    { connect = fun () -> Sql.connect connectionString
+    }
 
   0
